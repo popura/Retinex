e3b8f9fac351f283660084297010f8d3ccab2aca
Yuma Kinoshita <ykinoshita@tmu.ac.jp>
2021-06-15 17:22:17 +0900

    proc: implemented argparse

--- run.py
+++ run.py
@@ -1,25 +1,43 @@
+import argparse
 import sys
 import os
+from pathlib import Path
 
 import cv2
 import json
 
 import retinex
 
-data_path = 'data'
-img_list = os.listdir(data_path)
-if len(img_list) == 0:
-    print 'Data directory is empty.'
-    exit()
 
-with open('config.json', 'r') as f:
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--data_dir",
+    type=str,
+    default="./data",
+    help="Directory path for searching input images"
+)
+parser.add_argument(
+    "--result_dir",
+    type=str,
+    default="./result",
+    help="Directory path for saving resulting images"
+)
+parser.add_argument(
+    "--config",
+    type=str,
+    default="./config.json",
+    help="Path to config file"
+)
+
+args = parser.parse_args()
+data_dir = Path.cwd() /args.data_dir
+result_dir = Path.cwd() /args.result_dir
+
+with open(args.config, 'r') as f:
     config = json.load(f)
 
-for img_name in img_list:
-    if img_name == '.gitkeep':
-        continue
-    
-    img = cv2.imread(os.path.join(data_path, img_name))
+for q in data_dir.glob("*.png"):
+    img = cv2.imread(str(q))
 
     img_msrcr = retinex.MSRCR(
         img,
@@ -45,8 +63,7 @@ for img_name in img_list:
     )    
 
     shape = img.shape
-    cv2.imshow('Image', img)
-    cv2.imshow('retinex', img_msrcr)
-    cv2.imshow('Automated retinex', img_amsrcr)
-    cv2.imshow('MSRCP', img_msrcp)
-    cv2.waitKey()
+    cv2.imwrite(str(result_dir / 'Image' / f"{q.stem}.png"), img)
+    cv2.imwrite(str(result_dir / 'retinex' / f"{q.stem}.png"), img_msrcr)
+    cv2.imwrite(str(result_dir / 'Automated retinex' / f"{q.stem}.png"), img_amsrcr)
+    cv2.imwrite(str(result_dir / 'MSRCP' / f"{q.stem}.png"), img_msrcp)
